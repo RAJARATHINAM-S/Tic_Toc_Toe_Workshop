48e1999976825d6f74f8ea967da5246829ab348f
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-10-23 09:40:06 +0530

    Add project files.

---
+++ Tic_Toc_Toe_Workshop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tic_Toc_Toe_Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+
+            UC1_CreateBoard.Create_Board();
+           // Console.WriteLine();
+            Console.ReadLine();
+       }
+    }
+}
---
+++ Tic_Toc_Toe_Workshop/UC1_CreateBoard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Toc_Toe_Workshop
+{
+   public  class UC1_CreateBoard
+    {
+
+
+       
+        public static char[] Create_Board()
+        {
+            char[] board = new char[10];
+                for(int i = 0; i < board.Length; i++)
+            {
+                board[i] =' ';
+            }
+            return board;
+        }
+    }
+}
